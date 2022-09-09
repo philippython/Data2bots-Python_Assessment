bab30a99420ebfa7caf25ccc19942f2f9c23d01d
odulaja philip <odulajaphilip@gmail.com>
2022-09-09 02:43:56 +0100

    created filepath validator

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ from utility import num_to_words, type_checker
 
 """
 
+
 """
-with open('data/data_2.json') as dataset:
-    data = json.load(dataset)
-    message_attrs = data.get("message")
-    output = {}
-    key_num = 0
-    for attr, value in message_attrs.items():
-        key_num += 1
-        output[f"key_{num_to_words[key_num]}"] = {"type": type_checker(value),
-                                                    "tag": "",
-                                                    "description": "",
-                                                    "required": False
-                                                }
-    output_dataset = json.dumps(output, indent=4)
-    with open('schema/schema_2.json','w') as output_data:
-         output_data.write(output_dataset)
+def value__mapper(input__filepath, output__filepath) -> str:
     
+    with open(input__filepath) as dataset:
+        data = json.load(dataset)
+        message_attrs = data.get("message")
+        output = {}
+        key_num = 0
+        for attr, value in message_attrs.items():
+            key_num += 1
+            output[f"key_{num_to_words[key_num]}"] = {"type": type_checker(value),
+                                                        "tag": "",
+                                                        "description": "",
+                                                        "required": False
+                                                    }
+        output_dataset = json.dumps(output, indent=4)
+        with open(output__filepath,'w') as output_data:
+            output_data.write(output_dataset)
+        
+
+value__mapper()
