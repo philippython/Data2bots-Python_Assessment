6e66d2087cac30e5f84b5feb2273a21b30ea1418
odulaja philip <odulajaphilip@gmail.com>
2022-09-09 01:46:26 +0100

    created number to words file

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import json
-import inflect
-
-p = inflect.engine()
 
 with open('data/data_2.json') as dataset:
     data = json.load(dataset)
@@ -10,11 +7,19 @@ with open('data/data_2.json') as dataset:
     key_num = 0
     for attr, value in message_attrs.items():
         key_num += 1
-        type = "string" if isinstance(value, str)  else ""
-        type = "integer" if isinstance(value, int)  else ""
-        type = "enum" if isinstance(value, list) else ""
-        type = "array" if instance(value, dict) else ""
-        output[f"key_{p.number_to_words(key_num)}"] = {"type": type,
+        if isinstance(value, str):
+            type = "string"   
+        elif isinstance(value, int):
+            print(value)
+            type = "integer"
+        elif isinstance(value, list):
+            type = "enum" if all(isinstance(x, str) for x in value) and value != [] else ""
+                
+        elif isinstance(value, dict):
+            type = "array"
+        else:
+            ""
+        output[f"key_{key_num}"] = {"type": type,
                                     "tag": "",
                                     "description": "",
                                     "required": False
