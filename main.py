a28910542d467b698919bb32ed9ea491b4eaed50
odulaja philip <odulajaphilip@gmail.com>
2022-09-09 01:05:15 +0100

    installed inflect module

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import json
+import inflect
 
-with open('data/data_1.json', 'r') as dataset:
-    data = json.loads(str(dataset.readline()))
-    print(data)
+p = inflect.engine()
+
+with open('data/data_2.json') as dataset:
+    data = json.load(dataset)
+    message_attrs = data.get("message")
+    output = {}
+    key_num = 0
+    for attr, value in message_attrs.items():
+        key_num += 1
+        type = "string" if isinstance(value, str)  else ""
+        type = "integer" if isinstance(value, int)  else ""
+        type = "enum" if isinstance(value, list) else ""
+        type = "array" if instance(value, dict) else ""
+        output[f"key_{p.number_to_words(key_num)}"] = {"type": type,
+                                    "tag": "",
+                                    "description": "",
+                                    "required": False
+                                    }
+    print(output)                            
+    
