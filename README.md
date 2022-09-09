e81d2001c3a8be4610d2b15212d7df5077c05b67
odulaja philip <odulajaphilip@gmail.com>
2022-09-09 07:05:49 +0100

    commented main.py

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Assesment completed
 
-- View solution at this github repo
+- View solution at this github repo https://github.com/philippython/Data2bots-Python_Assessment
 - Datatype to be returned when we have Boolean Values wasn't specified but i represent them as null
 - No dependecies needed to run this code seamlessly but i have a requirements.txt file
 - Unittesting completed.
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 from utility import num_to_words, type_checker, filepath__validator
 
 """
-1. utility is a module i created for this project 
+1. utility is a module I created for this project 
 2. num_to_words is a dictionary imported from utility module simply converts a number to a word.
 3. type_checker function helps check the data type of each values of the message attributes.
 4. filepath__validator helps validate the output and input filepath which are parameters to the main function value__mapper.
@@ -10,20 +10,27 @@ from utility import num_to_words, type_checker, filepath__validator
 """
 
 def value__mapper(input__filepath, output__filepath) -> str:
+    # checks if filepath is valid
     if filepath__validator(input__filepath) and filepath__validator(output__filepath):
+        # Get input json from input__filepath
         with open(input__filepath) as dataset:
             data = json.load(dataset)
-            message_attrs = data.get("message")
+            # Gets alls all attributes of the message and returns empty dicitonary if message key does not exist
+            message_attrs = data.get("message", {})
             output = {}
-            key_num = 0
+            attr_num = 0
+            # iterates over attributes of message keys checking for data type.
             for attr, value in message_attrs.items():
-                key_num += 1
-                output[f"key_{num_to_words[key_num]}"] = {"type": type_checker(value),
+                # attr_num is used to keep track of current iteration
+                attr_num += 1
+                output[f"key_{num_to_words[attr_num]}"] = {"type": type_checker(value),
                                                             "tag": "",
                                                             "description": "",
                                                             "required": False
                                                         }
+            # creates json to be dumped in the output__filepath
             output_dataset = json.dumps(output, indent=4)
+            # writes to the output__filepath
             with open(output__filepath,'w') as output_data:
                 output_data.write(output_dataset)
             
